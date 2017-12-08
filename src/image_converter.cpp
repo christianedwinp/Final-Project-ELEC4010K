0020075a6ac1e3c7d99353b62af00ba4cea5bc45
jwicaksana <jwicaksana@connect.ust.hk>
2017-12-09 01:45:45 +0800

    test

--- src/image_converter.cpp
+++ src/image_converter.cpp
@@ -1,22 +1,36 @@
+/**************************************************/
+// WINDOW SIZE 510 x 510 Quadran 4
+// Ideally we want to keep the iamge in the middle, say 250*250
+/**************************************************/
+/////FIXX: FIX THE SUBSCRIBE TO AUTO CONTROL MODE
 #include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
-#include <opencv2/features2d/features2d.hpp>
 #include <opencv2/core/core.hpp>
-#include <opencv2/nonfree/features2d.hpp>
+
 #include "opencv2/features2d/features2d.hpp"
+//For Debugging
+#include "std_msgs/String.h"
+#include "std_msgs/Bool.h"
+#include <sstream>
+
 
 using namespace cv;
 //image window name
 static const std::string OPENCV_WINDOW = "Image window";
 static const std::string OPENCV_WINDOW2 = "Image window2";
 
-using namespace cv;
 Mat img_1, img_2, img_3, img_4, img_5;
+
+
+bool auto_control_condition = false;
+int counter = 0;
+float x_pos, y_pos, orientation;
 
 class ImageConverter
 {
@@ -25,6 +39,14 @@ class ImageConverter
   image_transport::Subscriber image_sub_;
   image_transport::Publisher image_pub_;
 
+  /****************/
+  ros::Publisher cmd_vel;
+  geometry_msgs::Twist vel;
+  /***************/
+  // For debugging
+  ros::Publisher chatter_debug;
+  /***************/
+
 public:
   ImageConverter()
     : it_(nh_)
@@ -32,7 +54,9 @@ public:
     //Subscribe to VREP image and publish converted image
     image_sub_ = it_.subscribe("/vrep/image", 1, &ImageConverter::imageCb, this);
     image_pub_ = it_.advertise("/rosopencv_interface/image", 1);
-
+    
+    cmd_vel = nh_.advertise<geometry_msgs::Twist>("/vrep/cmd_vel", 1);
+    chatter_debug = nh_.advertise<std_msgs::String>("/debugging",1000);  
     //OpenCV HighGUI calls to create a display window on start-up
     cv::namedWindow(OPENCV_WINDOW);
     cv::namedWindow(OPENCV_WINDOW2);
@@ -45,91 +69,91 @@ public:
     cv::destroyWindow(OPENCV_WINDOW2);
   }
 
-  cv::Mat ImageDetector(const cv::Mat flippedImage)
-  {
-  	//-- Step 1: Detect the keypoints using SURF Detector
-    int minHessian = 400;
-    SurfFeatureDetector detector( minHessian );
-    std::vector<KeyPoint> keypoints_1, keypoints_2, keypoints_3, keypoints_4, keypoints_5, keypoints_stream;
-    detector.detect( img_1, keypoints_1 );
-    // detector.detect( img_2, keypoints_2 );
-    // detector.detect( img_3, keypoints_3 );
-    // detector.detect( img_4, keypoints_4 );
-    // detector.detect( img_5, keypoints_5 );
-    detector.detect( flippedImage, keypoints_stream );
+  // cv::Mat ImageDetector(const cv::Mat flippedImage)
+  // {
+  // 	//-- Step 1: Detect the keypoints using SURF Detector
+  //   int minHessian = 400;
+  //   SurfFeatureDetector detector( minHessian );
+  //   std::vector<KeyPoint> keypoints_1, keypoints_2, keypoints_3, keypoints_4, keypoints_5, keypoints_stream;
+  //   detector.detect( img_1, keypoints_1 );
+  //   // detector.detect( img_2, keypoints_2 );
+  //   // detector.detect( img_3, keypoints_3 );
+  //   // detector.detect( img_4, keypoints_4 );
+  //   // detector.detect( img_5, keypoints_5 );
+  //   detector.detect( flippedImage, keypoints_stream );
 
-    //-- Step 2: Calculate descriptors (feature vectors)
-    // cv::Ptr<cv::DescriptorExtractor> extractor = new cv::SurfDescriptorExtractor;
-    SurfDescriptorExtractor extractor;
-    Mat descriptors_1, descriptors_2, descriptors_3, descriptors_4, descriptors_5, descriptors_stream;
-    extractor.compute( img_1, keypoints_1, descriptors_1 );
-    // extractor.compute( img_2, keypoints_2, descriptors_2 );
-    // extractor.compute( img_3, keypoints_3, descriptors_3 );
-    // extractor.compute( img_4, keypoints_4, descriptors_4 );
-    // extractor.compute( img_5, keypoints_5, descriptors_5 );
-    extractor.compute( flippedImage, keypoints_stream, descriptors_stream );
+  //   //-- Step 2: Calculate descriptors (feature vectors)
+  //   // cv::Ptr<cv::DescriptorExtractor> extractor = new cv::SurfDescriptorExtractor;
+  //   SurfDescriptorExtractor extractor;
+  //   Mat descriptors_1, descriptors_2, descriptors_3, descriptors_4, descriptors_5, descriptors_stream;
+  //   extractor.compute( img_1, keypoints_1, descriptors_1 );
+  //   // extractor.compute( img_2, keypoints_2, descriptors_2 );
+  //   // extractor.compute( img_3, keypoints_3, descriptors_3 );
+  //   // extractor.compute( img_4, keypoints_4, descriptors_4 );
+  //   // extractor.compute( img_5, keypoints_5, descriptors_5 );
+  //   extractor.compute( flippedImage, keypoints_stream, descriptors_stream );
 
-    // //-- Step 3: Matching descriptor vectors using FLANN matcher 
-    FlannBasedMatcher matcher;
-    //matching process pic001.jpg
-    std::vector< DMatch > matches;
-    matcher.match( descriptors_1, descriptors_stream, matches );
+  //   // //-- Step 3: Matching descriptor vectors using FLANN matcher 
+  //   FlannBasedMatcher matcher;
+  //   //matching process pic001.jpg
+  //   std::vector< DMatch > matches;
+  //   matcher.match( descriptors_1, descriptors_stream, matches );
 
-    // //-- Quick calculation of max and min distances between keypoints_1
-    double max_dist = 0; double min_dist = 100;
-    for( int i = 0; i < descriptors_1.rows; i++ ){ 
-      double dist = matches[i].distance;
-      if( dist < min_dist ) min_dist = dist;
-      if( dist > max_dist ) max_dist = dist;
-    }
-    printf("-- Max dist : %f \n", max_dist );
-    printf("-- Min dist : %f \n", min_dist );
+  //   // //-- Quick calculation of max and min distances between keypoints_1
+  //   double max_dist = 0; double min_dist = 100;
+  //   for( int i = 0; i < descriptors_1.rows; i++ ){ 
+  //     double dist = matches[i].distance;
+  //     if( dist < min_dist ) min_dist = dist;
+  //     if( dist > max_dist ) max_dist = dist;
+  //   }
+  //   printf("-- Max dist : %f \n", max_dist );
+  //   printf("-- Min dist : %f \n", min_dist );
 
-    // //-- Get good matches pic001.jpg
-    std::vector< DMatch > good_matches;
-    for( int i = 0; i < descriptors_1.rows; i++ ){ 
-      if( matches[i].distance < 3*min_dist ){ 
-        good_matches.push_back( matches[i]);
-      }
-    }
+  //   // //-- Get good matches pic001.jpg
+  //   std::vector< DMatch > good_matches;
+  //   for( int i = 0; i < descriptors_1.rows; i++ ){ 
+  //     if( matches[i].distance < 3*min_dist ){ 
+  //       good_matches.push_back( matches[i]);
+  //     }
+  //   }
     
-    Mat img_matches;
-    drawMatches( img_1, keypoints_1, flippedImage, keypoints_stream,
-               good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
-               vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+  //   Mat img_matches;
+  //   drawMatches( img_1, keypoints_1, flippedImage, keypoints_stream,
+  //              good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+  //              vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
 
 
-    // //-- Localize pic001.jpg
-    std::vector<Point2f> obj1;
-    std::vector<Point2f> stream;
-    for( int i = 0; i < good_matches.size(); i++ ){
-        //-- Get the keypoints from the good matches
-        obj1.push_back( keypoints_1[ good_matches[i].queryIdx ].pt );
-        stream.push_back( keypoints_stream[ good_matches[i].trainIdx ].pt );
-    }
-    Mat H = findHomography( obj1, stream, CV_RANSAC );
+  //   // //-- Localize pic001.jpg
+  //   std::vector<Point2f> obj1;
+  //   std::vector<Point2f> stream;
+  //   for( int i = 0; i < good_matches.size(); i++ ){
+  //       //-- Get the keypoints from the good matches
+  //       obj1.push_back( keypoints_1[ good_matches[i].queryIdx ].pt );
+  //       stream.push_back( keypoints_stream[ good_matches[i].trainIdx ].pt );
+  //   }
+  //   Mat H = findHomography( obj1, stream, CV_RANSAC );
 
-    // //-- Get the corners from the pic001.jpg
-    std::vector<Point2f> obj1_corners(4);
-    obj1_corners[0] = cvPoint(0,0); 
-    obj1_corners[1] = cvPoint( img_1.cols, 0 );
-    obj1_corners[2] = cvPoint( img_1.cols, img_1.rows ); 
-    obj1_corners[3] = cvPoint( 0, img_1.rows );
-    std::vector<Point2f> stream_corners(4);
+  //   // //-- Get the corners from the pic001.jpg
+  //   std::vector<Point2f> obj1_corners(4);
+  //   obj1_corners[0] = cvPoint(0,0); 
+  //   obj1_corners[1] = cvPoint( img_1.cols, 0 );
+  //   obj1_corners[2] = cvPoint( img_1.cols, img_1.rows ); 
+  //   obj1_corners[3] = cvPoint( 0, img_1.rows );
+  //   std::vector<Point2f> stream_corners(4);
 
-    perspectiveTransform( obj1_corners, stream_corners, H);
+  //   perspectiveTransform( obj1_corners, stream_corners, H);
 
-    // //-- Draw pic001.jpg border line in camera stream
-    line( img_matches, stream_corners[0] + Point2f( img_1.cols, 0), stream_corners[1] + Point2f( img_1.cols, 0), Scalar(0, 255, 0), 4 );
-    line( img_matches, stream_corners[1] + Point2f( img_1.cols, 0), stream_corners[2] + Point2f( img_1.cols, 0), Scalar( 0, 255, 0), 4 );
-    line( img_matches, stream_corners[2] + Point2f( img_1.cols, 0), stream_corners[3] + Point2f( img_1.cols, 0), Scalar( 0, 255, 0), 4 );
-    line( img_matches, stream_corners[3] + Point2f( img_1.cols, 0), stream_corners[0] + Point2f( img_1.cols, 0), Scalar( 0, 255, 0), 4 );
+  //   // //-- Draw pic001.jpg border line in camera stream
+  //   line( img_matches, stream_corners[0] + Point2f( img_1.cols, 0), stream_corners[1] + Point2f( img_1.cols, 0), Scalar(0, 255, 0), 4 );
+  //   line( img_matches, stream_corners[1] + Point2f( img_1.cols, 0), stream_corners[2] + Point2f( img_1.cols, 0), Scalar( 0, 255, 0), 4 );
+  //   line( img_matches, stream_corners[2] + Point2f( img_1.cols, 0), stream_corners[3] + Point2f( img_1.cols, 0), Scalar( 0, 255, 0), 4 );
+  //   line( img_matches, stream_corners[3] + Point2f( img_1.cols, 0), stream_corners[0] + Point2f( img_1.cols, 0), Scalar( 0, 255, 0), 4 );
 
-    // Update GUI Window
-    cv::imshow(OPENCV_WINDOW, img_matches);
-    return img_matches;
+  //   // Update GUI Window
+  //   cv::imshow(OPENCV_WINDOW, img_matches);
+  //   return img_matches;
     
-  }
+  // }
 
   //subscriber callback
   void imageCb(const sensor_msgs::ImageConstPtr& msg)
@@ -166,11 +190,11 @@ public:
     * flipcode < 0 -> flip on both axis
     */
     cv::flip(cv_ptr->image, flippedImage, 1);
-
+    cv::imshow(OPENCV_WINDOW, flippedImage);
     // =============================================
     // SURF FLANNMATCHER + HOMOGRAPHY
     // =============================================
-    cv::Mat output_detector = ImageDetector(flippedImage);
+    // cv::Mat output_detector = ImageDetector(flippedImage);
     // =============================================
     // BLOB TRACKING
     // =============================================
@@ -192,9 +216,8 @@ public:
     inRange(test, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded);
     //morphological opening (removes small objects from the foreground)
     erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-    dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
-
-     //morphological closing (removes small holes from the foreground)
+    dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+    //morphological closing (removes small holes from the foreground)
     dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
     erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
 
@@ -205,19 +228,73 @@ public:
     double dM10 = oMoments.m10;
     double dArea = oMoments.m00;
 
+    int posX = dM10 / dArea;
+    int posY = dM01 / dArea;
+    int lastX,lastY; 
+    /****************/
+    // Debugging
+    std_msgs::String debug_msg;
+    std::stringstream ss;
+    // Check ball detected or not
+    if (posY > 280) {
+      auto_control_condition = true;
+    }
+    if (dArea > 100000) {
+      ss << "xPos: " << posX << " Ypos: " << posY << " area : "<< dArea <<' '<< auto_control_condition;
+      debug_msg.data = ss.str();
+      chatter_debug.publish(debug_msg);
+      /*****************************/
+      // Perform Control
+      /* Hypothetically We split img to 4 quadrans + zero axis*/
+      /* We want to keep x & y in the middle which is about 250 */
+      /* Use 240 as Threshold Value */
+      /* If X  < 240 angle orient to left */
+      /* If X is in between angle stays */
+      /* If X > 260 angle orient to right */
+    
+      if (posX < 240) {
+        vel.angular.z = 0.6;
+      } else if (posX > 260) {
+        vel.angular.z = -0.6;
+      } else {
+        vel.angular.z = 0;
+      }
+
+      /* Y < 330 go forward */
+      /* Y > 360 slowdown */
+      if (posY < 330) {
+        vel.linear.x = 0.4;
+      } else if (posY > 360) {
+        vel.linear.x = -0.3;
+      } else {
+        vel.linear.x = 0.2;
+      }
+      
+      if(auto_control_condition) {
+        cmd_vel.publish(vel);
+      }
+      /*****************************/
+    }
+    /****************/
+    // IF LASTX & LASTY suddenly gone missing 
+
+    /***************/
+
     cv::imshow(OPENCV_WINDOW2, imgThresholded);
     cv::waitKey(3);
 
+    // Output modified image stream
+    image_pub_.publish(cv_ptr->toImageMsg());
     // =============================================
     // SEND BACK EDITED IMAGE IN CV TO ROS SENSOR MESSAGE
     // =============================================
-    cv_bridge::CvImage img_bridge;
-    sensor_msgs::Image img_msg;
-    std_msgs::Header header;
-    //change the output image as suitable
-    img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::BGR8, flippedImage); //convert CVimage to ROS
-    img_bridge.toImageMsg(img_msg); 
-    image_pub_.publish(img_msg); 
+    // cv_bridge::CvImage img_bridge;
+    // sensor_msgs::Image img_msg;
+    // std_msgs::Header header;
+    // //change the output image as suitable
+    // img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::BGR8, flippedImage); //convert CVimage to ROS
+    // img_bridge.toImageMsg(img_msg); 
+    // image_pub_.publish(img_msg); 
   }
 };
 
@@ -226,11 +303,11 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "rosopencv_interface");
   
   //LOAD IMAGE DATASET
-  if( argc != 2)
-    {
-     std::cout << "Missing arguments" << std::endl;
-     return -1;
-    }
+  // if( argc != 2)
+  //   {
+  //    std::cout << "Missing arguments" << std::endl;
+  //    return -1;
+  //   }
     
   // img_1 = imread( argv[1], CV_LOAD_IMAGE_COLOR ); //load pic001.jpg
   // img_2 = imread( argv[2], CV_LOAD_IMAGE_COLOR ); //load pic002.jpg
@@ -241,10 +318,10 @@ int main(int argc, char** argv)
   // if( !img_1.data || !img_2.data || !img_3.data || !img_4.data || !img_5.data )
   //     { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
 
-    img_1 = imread( argv[1], CV_LOAD_IMAGE_COLOR ); //load pic001.jpg
+  //   img_1 = imread( argv[1], CV_LOAD_IMAGE_COLOR ); //load pic001.jpg
 
-  if( !img_1.data )
-      { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
+  // if( !img_1.data )
+  //     { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
 
   ImageConverter ic;
   ros::spin();
